c5a3f6a4728c402eb906c16df38320027c5531f2
ssaha70 <ssaha70@asu.edu>
2025-05-28 08:36:41 +0000

    add

--- backend/app.py
+++ backend/app.py
@@ -47,8 +47,6 @@ def upload_file():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-    app.run(host="0.0.0.0", port=5000)
-=======
+
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
